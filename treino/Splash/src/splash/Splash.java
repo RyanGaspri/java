6bc164c554924ac14926c92204f16e17ceba342c
RyanGaspri <88353449+RyanGaspri@users.noreply.github.com>
2021-11-30 22:16:57 -0300

    Add files via upload

---
+++ treino/Splash/src/splash/Splash.java
@@ -0,0 +1,14 @@
+
+package splash;
+
+import View.Splash_GUI;
+
+public class Splash 
+{
+
+    public static void main(String[] args) 
+    {
+        new Splash_GUI().setVisible(true);
+    }
+    
+}
